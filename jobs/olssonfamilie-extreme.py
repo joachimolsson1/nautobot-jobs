5be7d7a2054d3734200e77c939c9e7e45d538a5f
joachimolsson1 <joachim@olssonfamilie.no>
2025-01-05 12:46:33 +0000

    test

--- jobs/olssonfamilie-extreme.py
+++ jobs/olssonfamilie-extreme.py
@@ -15,6 +15,9 @@ class FetchAndAddExtremeCloudIQDevices(Job):
     api_token = StringVar(
         description="API Token for ExtremeCloud IQ",
     )
+    tenant_name = StringVar(
+        description="tenant navn på kunden.",
+    )    
 
     def run(self, data, commit):
         api_token = data["api_token"]
