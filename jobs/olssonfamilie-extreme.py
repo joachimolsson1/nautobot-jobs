02c2ffa2fc6d1334cf4cb2e2c45264c51b3caf17
joachimolsson1 <joachim@olssonfamilie.no>
2025-01-05 18:21:31 +0000

    test

--- jobs/olssonfamilie-extreme.py
+++ jobs/olssonfamilie-extreme.py
@@ -6,7 +6,6 @@ from nautobot.tenancy.models import Tenant
 from nautobot.ipam.models import IPAddress
 from nautobot.extras.models import Status
 from nautobot.extras.jobs import BooleanVar, ChoiceVar, FileVar, Job, ObjectVar, RunJobTaskFailed, StringVar, TextVar
-from nautobot.apps.forms import DynamicModelChoiceField
 
 
 class FetchAndAddExtremeCloudIQDevices(Job):
@@ -58,7 +57,7 @@ class FetchAndAddExtremeCloudIQDevices(Job):
             device_type, _ = DeviceType.objects.get_or_create(model=device_model)
             status = Status.objects.get(name='Active')  # Adjust status as needed
 
-            tenant = Tenant.objects.get(name={tenant_name})
+            tenant = Tenant.objects.get(name=f'{tenant_name}')
             # Create or fetch location 
             location = Location.objects.get_or_create(
                 name=device["locations"][1],
