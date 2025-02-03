edae098d6123cf868328fe68a73a8a3df140f99b
joachimolsson1 <joachim@olssonfamilie.no>
2025-02-03 12:15:36 +0000

    test

--- jobs/PaloAltoNetworks.py
+++ jobs/PaloAltoNetworks.py
@@ -283,9 +283,7 @@ class FetchAndAddorUpdatePanoramaandFirewall(Job):
  
 
         #Firewall
-        device_role = Role.objects.filter(model="Firewall")
-        devices_firewall = Device.objects.filter(_custom_field_data__icontains={'Services':['Firewall as a Service']})
-
+        devices_firewall = Device.objects.filter(_custom_field_data__icontains='Firewall as a Service')
         for device_firewall in devices_firewall:
             device_name_string = str(device_firewall.name)
             #device_loopback_url = device_firewall.custom_field_data["loopback_url"]
